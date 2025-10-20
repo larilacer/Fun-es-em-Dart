391ddb103439627b209f8efed8e202743934ede6
larilacer <larissa.lacerda06@aluno.ifce.edu.br>
2025-10-20 04:41:03 -0300

    Adicionando exercícios sobre funções em Dart

---
+++ FuSoma.dart
@@ -0,0 +1,6 @@
+void main() {
+  int n1 = 3;
+  int n2 = 2;
+  int soma = n1 + n2;
+  print(soma);
+}
---
+++ FuncMedia.dart
@@ -0,0 +1,8 @@
+void main() {
+  int n1 = 3;
+  int n2 = 5;
+  int soma = n1 + n2;
+  int resultadodaS = soma;
+  double media = resultadodaS / 2;
+  print(media);
+}
